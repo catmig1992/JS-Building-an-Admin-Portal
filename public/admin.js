dba387fc37d6dd3f683aac8e5f5579e2c1a1b47a
Catherine Miguel <catmig1992@gmail.com>
2022-10-12 15:18:53 -0700

    in the works - fetch GET

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,14 @@
-
 // Your Code Here
+async function main() {
+  const response = await fetch("http://localhost:3001/listBooks");
+
+  const result = await response.json();
+
+  let listBooks = result;
+  async function listBooks() {
+    await fetch("http://localhost:3001/listBooks", {
+      method: "GET",
+    });
+  }
+}
+main();
